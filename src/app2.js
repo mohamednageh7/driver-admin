5fe5bb94440c66b6f23f835c126f337dc8ebc42c
unknown <albanza38@gamil.com>
2019-08-26 16:39:58 +0200

    comment dummy code

--- src/app2.js
+++ src/app2.js
@@ -11,9 +11,9 @@ import './style/style.scss';
 const store = configureStore(); // this will give us the access to everything in store ( store dispatch, subscribe, store getstate
 
 
- store.dispatch(addDrivers({description:'Gas Bill',amount:1300}));
- store.dispatch(addDrivers({description:'Water Bill', createdAt: 1000}));
- store.dispatch(addDrivers({description:'Rent', amount: 20500}));
+//  store.dispatch(addDrivers({description:'Gas Bill',amount:1300}));
+//  store.dispatch(addDrivers({description:'Water Bill', createdAt: 1000}));
+//  store.dispatch(addDrivers({description:'Rent', amount: 20500}));
 
 // store.dispatch(setTextFilter('gas'));
 
@@ -21,9 +21,9 @@ const store = configureStore(); // this will give us the access to everything in
 //     store.dispatch(setTextFilter('water'));
 // }, 3000)
 
-const state = store.getState();
-const visibilityDriver = visibleDriver(state.driverList, state.driverFilters);
-console.log(visibilityDriver);
+// const state = store.getState();
+// const visibilityDriver = visibleDriver(state.driverList, state.driverFilters);
+// console.log(visibilityDriver);
 
 
 const jsx =(
